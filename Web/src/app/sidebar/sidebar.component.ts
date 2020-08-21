91c3222d88972d82c8e6a2e0732b22afdb5265ca
ErickErraez <erickerraez2@gmail.com>
2020-08-21 18:49:52 -0500

    Create method log out to end session

--- Web/src/app/sidebar/sidebar.component.ts
+++ Web/src/app/sidebar/sidebar.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 declare const $: any;
+
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
+
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Principal',  icon: 'pe-7s-graph', class: '' },
+  {path: '/dashboard', title: 'Principal', icon: 'pe-7s-graph', class: ''},
 ];
 
 @Component({
@@ -18,15 +21,21 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor(private route: Router) {
+  }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
+
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
+
+  logout() {
+    this.route.navigate(['/login']);
+  }
 }
